d0aaa976b75af56f5a0c6e98cc13ce7c6da12742
NoahFay <31666397+NoahFay@users.noreply.github.com>
2017-10-13 13:15:11 -0400

    Update E97.java

--- lab_6/E97.java
+++ lab_6/E97.java
@@ -2,14 +2,12 @@ public class E97 {
 	public static void main(String[] args) {
 			
 		Account account = new Account(1122, 20000);
-			account.withdraw(2500);
-			account.deposit(3000);
-			account.setAnnualInterestRate(4.5);
-
-			
-			System.out.println("ID" + account.getid());
-			System.out.println("you got this account " + account.getDateCreated());
-			System.out.println("Balance" + account.getBalance());
-			System.out.println(" interest" + account.getMonthlyInterest());
+		account.withdraw(2500);
+		account.deposit(3000);
+		account.setAnnualInterestRate(4.5);
+		System.out.println("ID" + account.getid());
+		System.out.println("you got this account " + account.getDateCreated());
+		System.out.println("Balance" + account.getBalance());
+		System.out.println(" interest" + account.getMonthlyInterest());
 			
 	}}
