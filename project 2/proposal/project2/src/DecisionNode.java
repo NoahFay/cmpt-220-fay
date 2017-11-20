b6bababe03747d55f52fee4f852454844b06558c
NoahFay <31666397+NoahFay@users.noreply.github.com>
2017-11-20 13:05:52 -0500

    Update DecisionNode.java

--- project 2/proposal/project2/src/DecisionNode.java
+++ project 2/proposal/project2/src/DecisionNode.java
@@ -2,9 +2,8 @@ import java.io.FileWriter;
 import java.io.IOException;
 import java.util.Scanner;
 
-/**
- * the DecisionNode interface has three methods: countObjects, guess, and write (writes to a file).
- */
+ //  count Objects, guess, and writes to a file
+ 
 public interface DecisionNode {
 	public int countObjects();
 	public DecisionNode guess(Scanner in);
