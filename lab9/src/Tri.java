c1c40bbfdfdf6ee32e8ed247682290606fd431c3
Juan Arias <juan.arias@marist.edu>
2017-11-26 16:54:06 -0500

    Feedback

--- lab9/src/Tri.java
+++ lab9/src/Tri.java
@@ -15,7 +15,7 @@ public class Tri
 		if (side1 + side2 <= side3 || 
 			 side1 + side3 <= side2 || 
 			 side2 + side3 <= side1) 
-			throw new Exception();
+			throw new Exception(); // JA: This should throw TriException
 		this.side1 = side1;
 		this.side2 = side2;
 		this.side3 = side3;
